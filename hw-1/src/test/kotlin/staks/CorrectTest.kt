7653c793f4bc559be545cdb467d213bd452ed939
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-04-03 02:16:30 +0300

    refactor(hw-1): change delay to join, now launch runs on own thread

--- hw-1/src/test/kotlin/staks/CorrectTest.kt
+++ hw-1/src/test/kotlin/staks/CorrectTest.kt
@@ -1,44 +1,42 @@
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+package staks
+
+import kotlinx.coroutines.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotEquals
-import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Timeout
 import java.util.concurrent.atomic.AtomicInteger
-import kotlin.random.Random
 
 class CorrectTest {
-    private val seed = 52
-    private val threads = 20
+    private val threads = 10
     private val iterations = 10000
 
     @ParameterizedTest(name = "{1}")
     @ArgumentsSource(StackArguments::class)
     fun popAfterPushTest(stack: Stack<Int>, name: String) {
         val vals = Array(iterations * threads) { false }
+        val jobs = mutableListOf<Job>()
         runBlocking {
             repeat(threads) { curTh ->
-                launch {
+                jobs.add(launch(newSingleThreadContext(curTh.toString())) {
                     repeat(iterations) {
                         stack.push(it * threads + curTh)
                     }
-                }
+                })
             }
-            delay(5)
+            jobs.joinAll()
+            jobs.clear()
             repeat(threads) {
-                launch {
+                jobs.add(launch(newSingleThreadContext((threads + it).toString())) {
                     repeat(iterations) {
                         stack.pop()?.let { pos ->
                             assert(!vals[pos])
                             vals[pos] = true
                         }
                     }
-                }
+                })
             }
-            delay(5)
+            jobs.joinAll()
             assertEquals(null, stack.pop())
             for (it in vals) {
                 assert(it)
@@ -52,9 +50,10 @@ class CorrectTest {
     fun popAndPushOneThreadTest(stack: Stack<Int>, name: String) {
         val vals = Array(iterations * threads) { false }
         val succeedPop = AtomicInteger(0)
+        val jobs = mutableListOf<Job>()
         runBlocking {
             repeat(threads) { curTh ->
-                launch {
+                jobs.add(launch(newSingleThreadContext(curTh.toString())) {
                     repeat(iterations) {
                         stack.push(it * threads + curTh)
                         stack.pop()?.let { pos ->
@@ -63,10 +62,10 @@ class CorrectTest {
                             succeedPop.getAndIncrement()
                         }
                     }
-                }
+                })
             }
-            delay(5)
-            while (succeedPop.get() < iterations) {
+            jobs.joinAll()
+            while (succeedPop.get() < iterations*threads) {
                 stack.pop()?.let { pos ->
                     assert(!vals[pos])
                     vals[pos] = true
@@ -86,14 +85,15 @@ class CorrectTest {
     fun popAndPushTest(stack: Stack<Int>, name: String) {
         val succeedPop = AtomicInteger(0)
         val vals = Array(iterations * threads) { false }
+        val jobs = mutableListOf<Job>()
         runBlocking {
             repeat(threads) { curTh ->
-                launch {
+                jobs.add(launch(newSingleThreadContext(curTh.toString())) {
                     repeat(iterations) {
                         stack.push(it * threads + curTh)
                     }
-                }
-                launch {
+                })
+                jobs.add(launch(newSingleThreadContext((curTh + threads).toString())) {
                     repeat(iterations) {
                         stack.pop()?.let { pos ->
                             assert(!vals[pos])
@@ -101,10 +101,10 @@ class CorrectTest {
                             succeedPop.getAndIncrement()
                         }
                     }
-                }
+                })
             }
-            delay(5)
-            while (succeedPop.get() < iterations) {
+            jobs.joinAll()
+            while (succeedPop.get() < iterations*threads) {
                 stack.pop()?.let { pos ->
                     assert(!vals[pos])
                     vals[pos] = true
