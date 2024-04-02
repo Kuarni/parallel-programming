d97502f13b308e32349584334e877a06120d6aed
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-04-02 21:10:29 +0300

    feat(hw-1): improve correctness tests add poping data check

--- hw-1/src/test/kotlin/CorrectTest.kt
+++ hw-1/src/test/kotlin/CorrectTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Timeout
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.random.Random
@@ -19,24 +20,31 @@ class CorrectTest {
     fun popAfterPushTest(stack: Stack<Int>, name: String) {
         val threads = Random(seed).nextInt(1, maxThreads + 1)
         val iterations = Random(seed).nextInt(1, maxIter + 1)
+        val vals = Array(iterations * threads) { false }
         runBlocking {
-            repeat(iterations) {
-                repeat(threads) {
-                    launch {
-                        stack.push(it)
+            repeat(threads) { curTh ->
+                launch {
+                    repeat(iterations) {
+                        stack.push(it * threads + curTh)
                     }
                 }
             }
             delay(5)
-            repeat(iterations) {
-                repeat(threads) {
-                    launch {
-                        assertNotEquals(null, stack.pop())
+            repeat(threads) {
+                launch {
+                    repeat(iterations) {
+                        stack.pop()?.let { pos ->
+                            assert(!vals[pos])
+                            vals[pos] = true
+                        }
                     }
                 }
             }
             delay(5)
             assertEquals(null, stack.pop())
+            for (it in vals) {
+                assert(it)
+            }
         }
     }
 
@@ -46,21 +54,33 @@ class CorrectTest {
     fun popAndPushOneThreadTest(stack: Stack<Int>, name: String) {
         val threads = Random(seed).nextInt(1, maxThreads + 1)
         val iterations = Random(seed).nextInt(1, maxIter + 1)
-        var succeedPop = AtomicInteger(0)
+        val vals = Array(iterations * threads) { false }
+        val succeedPop = AtomicInteger(0)
         runBlocking {
-            repeat(iterations) {
-                repeat(threads) {
-                    launch {
-                        stack.push(it)
-                        if (stack.pop() != null) succeedPop.getAndIncrement()
+            repeat(threads) { curTh ->
+                launch {
+                    repeat(iterations) {
+                        stack.push(it * threads + curTh)
+                        stack.pop()?.let { pos ->
+                            assert(!vals[pos])
+                            vals[pos] = true
+                            succeedPop.getAndIncrement()
+                        }
                     }
                 }
             }
             delay(5)
             while (succeedPop.get() < iterations) {
-                if (stack.pop() != null) succeedPop.getAndIncrement()
+                stack.pop()?.let { pos ->
+                    assert(!vals[pos])
+                    vals[pos] = true
+                    succeedPop.getAndIncrement()
+                }
             }
             assertEquals(null, stack.pop())
+            for (it in vals) {
+                assert(it)
+            }
         }
     }
 
@@ -70,23 +90,37 @@ class CorrectTest {
     fun popAndPushTest(stack: Stack<Int>, name: String) {
         val threads = Random(seed).nextInt(1, maxThreads + 1)
         val iterations = Random(seed).nextInt(1, maxIter + 1)
-        var succeedPop = AtomicInteger(0)
+        val succeedPop = AtomicInteger(0)
+        val vals = Array(iterations * threads) { false }
         runBlocking {
-            repeat(iterations) {
-                repeat(threads) {
-                    launch {
-                        stack.push(it)
+            repeat(threads) { curTh ->
+                launch {
+                    repeat(iterations) {
+                        stack.push(it * threads + curTh)
                     }
-                    launch {
-                        if (stack.pop() != null) succeedPop.getAndIncrement()
+                }
+                launch {
+                    repeat(iterations) {
+                        stack.pop()?.let { pos ->
+                            assert(!vals[pos])
+                            vals[pos] = true
+                            succeedPop.getAndIncrement()
+                        }
                     }
                 }
             }
             delay(5)
             while (succeedPop.get() < iterations) {
-                if (stack.pop() != null) succeedPop.getAndIncrement()
+                stack.pop()?.let { pos ->
+                    assert(!vals[pos])
+                    vals[pos] = true
+                    succeedPop.getAndIncrement()
+                }
             }
             assertEquals(null, stack.pop())
+            for (it in vals) {
+                assert(it)
+            }
         }
     }
 }
