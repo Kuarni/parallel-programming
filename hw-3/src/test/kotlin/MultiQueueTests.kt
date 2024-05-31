bfeea7e6055d045dc574ef2d2c225ee24bc8ec50
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-05-31 10:08:04 +0300

    feat: update tests and add test for parallelDijkstra

---
+++ hw-3/src/test/kotlin/MultiQueueTests.kt
@@ -0,0 +1,39 @@
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.util.concurrent.atomic.AtomicBoolean
+
+class MultiQueueTest {
+    @Test
+    fun simpleTest() {
+        val threads = 2
+        val queues = 2
+        val iterations = 20
+        val queue = dijkstra.MultiQueue<Int>(queues)
+        val jobs = mutableListOf<Job>()
+        val data = Array(threads * iterations) { iterations * threads - it }
+        val ans = Array(threads * iterations) { AtomicBoolean(false) }
+        runBlocking {
+            repeat(threads) { curTh ->
+                jobs.add(launch(newSingleThreadContext(curTh.toString())) {
+                    repeat(iterations) {
+                        queue.add(data[it * threads + curTh])
+                    }
+                })
+            }
+            jobs.joinAll()
+            jobs.clear()
+            repeat(threads) { curTh ->
+                jobs.add(launch(newSingleThreadContext(curTh.toString())) {
+                    while (true) {
+                        val result = queue.poll() ?: break
+                        assertTrue { ans[result - 1].compareAndSet(false, true) }
+                    }
+                })
+            }
+            jobs.joinAll()
+            for (i in ans)
+                assertTrue(i.get())
+        }
+    }
+}
---
+++ hw-3/src/test/kotlin/SeqDijkstraTest.kt
@@ -0,0 +1,18 @@
+import dijkstra.dijkstraSeq
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SeqDijkstraTest {
+    @Test
+    fun simpleTest() {
+        val graph: WeightedGraph = hashMapOf(
+            "a" to arrayOf("b" dist 3, "c" dist 10),
+            "b" to arrayOf("a" dist 3, "d" dist 100, "c" dist 2),
+            "c" to arrayOf("a" dist 10, "b" dist 2, "d" dist 5, "e" dist 1),
+            "e" to arrayOf("c" dist 1, "d" dist 1),
+            "d" to arrayOf("b" dist 100, "c" dist 5, "e" dist 1),
+        )
+
+        assertEquals(7, dijkstraSeq(graph, "a", "d"))
+    }
+}
