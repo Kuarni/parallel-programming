ada555c9bf797323d9a74ceaed72f7e2a2997073
Arseniy Kuznetsov <Kuarni22@gmail.com>
2024-05-31 12:11:48 +0300

    feat: add parallel Floyd

--- hw-3/src/main/kotlin/floyd/Floyd.kt
+++ hw-3/src/main/kotlin/floyd/Floyd.kt
@@ -1,8 +1,12 @@
 package floyd
 
 import WeightedGraph
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlin.math.min
 
-fun floydSeq(graph: WeightedGraph, start: String, destination: String): Int {
+fun floyd(graph: WeightedGraph, start: String, destination: String, ijPart: (Array<Array<Long>>, Int) -> Unit): Int {
     var counter = 0
     var startInt = 0
     var destinationInt = 0
@@ -20,6 +24,14 @@ fun floydSeq(graph: WeightedGraph, start: String, destination: String): Int {
         }
     }
     for (k in dist.indices) {
+        ijPart(dist, k)
+    }
+
+    return dist[startInt][destinationInt].toInt()
+}
+
+fun floydSeq(graph: WeightedGraph, start: String, destination: String): Int =
+    floyd(graph, start, destination) { dist: Array<Array<Long>>, k: Int ->
         for (i in dist.indices) {
             for (j in dist.indices) {
                 if (dist[i][j] > dist[i][k] + dist[k][j]) {
@@ -29,5 +41,23 @@ fun floydSeq(graph: WeightedGraph, start: String, destination: String): Int {
         }
     }
 
-    return dist[startInt][destinationInt].toInt()
-}
+fun floydParallel(graph: WeightedGraph, start: String, destination: String, numThreads: Int = 4): Int =
+    floyd(graph, start, destination) { dist: Array<Array<Long>>, k: Int ->
+        val segment = dist.size / numThreads +  if (dist.size % numThreads != 0) 1 else 0
+        runBlocking {
+            repeat(numThreads) {
+                val segmentMin = it * segment
+                val segmentMax = min((it + 1) * segment, dist.size)
+                if (segmentMin < dist.size)
+                    launch(newSingleThreadContext(it.toString())) {
+                        for (i in segmentMin until segmentMax) {
+                            for (j in dist.indices) {
+                                if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                                    dist[i][j] = dist[i][k] + dist[k][j]
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+    }
---
+++ hw-3/src/test/kotlin/floyd/ParallelFloyd.kt
@@ -0,0 +1,21 @@
+package floyd
+
+import WeightedGraph
+import dist
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ParallelFloyd {
+    @Test
+    fun simpleTest() {
+        val graph: WeightedGraph = hashMapOf(
+            "a" to arrayOf("b" dist 3, "c" dist 10),
+            "b" to arrayOf("a" dist 3, "d" dist 100, "c" dist 2),
+            "c" to arrayOf("a" dist 10, "b" dist 2, "d" dist 5, "e" dist 1),
+            "e" to arrayOf("c" dist 1, "d" dist 1),
+            "d" to arrayOf("b" dist 100, "c" dist 5, "e" dist 1),
+        )
+
+        assertEquals(7, floydParallel(graph, "a", "d"))
+    }
+}
